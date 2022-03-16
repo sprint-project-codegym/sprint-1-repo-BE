06d9531785be48865bc281db4789662bafadaba1
nguyendinhhunganh <nguyendinhhunganh99@gmail.com>
2022-03-16 21:15:26 +0700

    AnhNDH push code 16/3

--- BE/src/main/java/com/example/sprint_1/entity/contract/Contract.java
+++ BE/src/main/java/com/example/sprint_1/entity/contract/Contract.java
@@ -13,8 +13,8 @@ import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
-//@Setter
-//@Getter
+@Setter
+@Getter
 @Entity(name = "contract")
 public class Contract {
     @Id
@@ -42,91 +42,91 @@ public class Contract {
     @JoinColumn(name = "groundId")
     private Ground ground;
 
-    public String getContractId() {
-        return contractId;
-    }
-
-    public void setContractId(String contractId) {
-        this.contractId = contractId;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getContractDate() {
-        return contractDate;
-    }
-
-    public void setContractDate(String contractDate) {
-        this.contractDate = contractDate;
-    }
-
-    public Double getRentCost() {
-        return rentCost;
-    }
-
-    public void setRentCost(Double rentCost) {
-        this.rentCost = rentCost;
-    }
-
-    public Double getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(Double totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public String getContractContent() {
-        return contractContent;
-    }
-
-    public void setContractContent(String contractContent) {
-        this.contractContent = contractContent;
-    }
-
-    public Boolean getDeleteFlag() {
-        return deleteFlag;
-    }
-
-    public void setDeleteFlag(Boolean deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public Employee getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    public Ground getGround() {
-        return ground;
-    }
-
-    public void setGround(Ground ground) {
-        this.ground = ground;
-    }
+//    public String getContractId() {
+//        return contractId;
+//    }
+//
+//    public void setContractId(String contractId) {
+//        this.contractId = contractId;
+//    }
+//
+//    public String getStartDate() {
+//        return startDate;
+//    }
+//
+//    public void setStartDate(String startDate) {
+//        this.startDate = startDate;
+//    }
+//
+//    public String getEndDate() {
+//        return endDate;
+//    }
+//
+//    public void setEndDate(String endDate) {
+//        this.endDate = endDate;
+//    }
+//
+//    public String getContractDate() {
+//        return contractDate;
+//    }
+//
+//    public void setContractDate(String contractDate) {
+//        this.contractDate = contractDate;
+//    }
+//
+//    public Double getRentCost() {
+//        return rentCost;
+//    }
+//
+//    public void setRentCost(Double rentCost) {
+//        this.rentCost = rentCost;
+//    }
+//
+//    public Double getTotalCost() {
+//        return totalCost;
+//    }
+//
+//    public void setTotalCost(Double totalCost) {
+//        this.totalCost = totalCost;
+//    }
+//
+//    public String getContractContent() {
+//        return contractContent;
+//    }
+//
+//    public void setContractContent(String contractContent) {
+//        this.contractContent = contractContent;
+//    }
+//
+//    public Boolean getDeleteFlag() {
+//        return deleteFlag;
+//    }
+//
+//    public void setDeleteFlag(Boolean deleteFlag) {
+//        this.deleteFlag = deleteFlag;
+//    }
+//
+//    public Customer getCustomer() {
+//        return customer;
+//    }
+//
+//    public void setCustomer(Customer customer) {
+//        this.customer = customer;
+//    }
+//
+//    public Employee getEmployee() {
+//        return employee;
+//    }
+//
+//    public void setEmployee(Employee employee) {
+//        this.employee = employee;
+//    }
+//
+//    public Ground getGround() {
+//        return ground;
+//    }
+//
+//    public void setGround(Ground ground) {
+//        this.ground = ground;
+//    }
 }
