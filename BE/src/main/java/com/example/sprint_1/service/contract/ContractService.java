65e0290fe0550544eebbc88a786cda8a3d651ef0
nguyendinhhunganh <nguyendinhhunganh99@gmail.com>
2022-03-16 21:04:08 +0700

    AnhNDH push code 16/3

--- BE/src/main/java/com/example/sprint_1/service/contract/ContractService.java
+++ BE/src/main/java/com/example/sprint_1/service/contract/ContractService.java
@@ -1,4 +1,24 @@
 package com.example.sprint_1.service.contract;
 
+import com.example.sprint_1.entity.contract.Contract;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ContractService {
+    List<Contract> findAll(); //Get all contract
+
+    void deleteContractById(String id); //Delete contract by id
+
+    Contract findById(String id); //Find contract by id
+
+    List<Contract> filterContract(List<Contract> contracts); // filter contract have delete_flag = 1
+
+    List<Contract> findByIdAndCustomerName(String id, String customerName); //search contract by id and customername
+
+    List<Contract> findByIdContaining(String id); //search contract by id
+
+    List<Contract> findByCustomerName(String customerName); //seach contract by customer name
+
 }
--- BE/src/main/java/com/example/sprint_1/service/impl/contract/ContractServiceImpl.java
+++ BE/src/main/java/com/example/sprint_1/service/impl/contract/ContractServiceImpl.java
@@ -1,8 +1,69 @@
 package com.example.sprint_1.service.impl.contract;
 
+import com.example.sprint_1.entity.contract.Contract;
+import com.example.sprint_1.repository.contract.ContractRepository;
 import com.example.sprint_1.service.contract.ContractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ContractServiceImpl implements ContractService {
+    @Autowired
+    ContractRepository contractRepository;
+
+    @Override
+    public List<Contract> findAll() {
+
+        List<Contract> contracts = contractRepository.findAllContract();
+        return filterContract(contracts);
+    }
+
+    @Override
+    public void deleteContractById(String id) {
+        contractRepository.deleteContract(id);
+    }
+
+    @Override
+    public Contract findById(String id) {
+        return contractRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Contract> filterContract(List<Contract> contracts) {
+        List<Contract> contractList = new ArrayList<>();
+        for (Contract contract : contracts) {
+            if (contract.getDeleteFlag()) {
+                contractList.add(contract);
+            }
+        }
+        return contractList;
+    }
+
+    //
+    @Override
+    public List<Contract> findByIdAndCustomerName(String id, String customerName) {
+        List<Contract> contracts = contractRepository.findByContractIdContainingAndCustomerNameContaining(id, customerName);
+        return filterContract(contracts);
+    }
+
+    @Override
+    public List<Contract> findByIdContaining(String id) {
+        List<Contract> contracts = contractRepository.findByContractIdContaining(id);
+        return filterContract(contracts);
+    }
+
+    @Override
+    public List<Contract> findByCustomerName(String customerName) {
+        List<Contract> contracts = contractRepository.findByCustomerNameContaining(customerName);
+        return filterContract(contracts);
+    }
+
+//    @Override
+//    public List<Contract> searchContract() {
+//        List<Contract> contracts = contractRepository.searchContract();
+//        return contracts;
+//    }
 }
