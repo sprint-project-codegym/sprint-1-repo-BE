10bd181cd762ce6b6d66a9fb25454371d3508351
nguyendinhhunganh <nguyendinhhunganh99@gmail.com>
2022-03-20 20:40:17 +0700

    AnhNDH push code 20/3

--- BE/src/main/java/com/example/sprint_1/controller/ContractController.java
+++ BE/src/main/java/com/example/sprint_1/controller/ContractController.java
@@ -3,6 +3,9 @@ package com.example.sprint_1.controller;
 import com.example.sprint_1.entity.contract.Contract;
 import com.example.sprint_1.service.contract.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,20 +22,41 @@ public class ContractController {
     ContractService contractService;
 
     @GetMapping("/list") // Get list contract and search//
-    public ResponseEntity<List<Contract>> listAllContract(@RequestParam("id") Optional<String> id, @RequestParam("customerName") Optional<String> customerName) {
-        List<Contract> contracts = contractService.findAll();
-        if(id.isPresent() && !customerName.isPresent()) {
-            contracts = contractService.findByIdContaining(id.get());
-        } else if(!id.isPresent() && customerName.isPresent()) {
-            contracts = contractService.findByCustomerName(customerName.get());
-        } else if(id.isPresent()) {
-            contracts = contractService.findByIdAndCustomerName(id.get(),customerName.get());
+    public ResponseEntity<Page<Contract>> GetListWithPagination(@RequestParam(defaultValue = "") String id,
+                                                                @RequestParam(defaultValue = "") String customerName,
+                                                                @RequestParam(defaultValue = "0") int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Contract> contracts;
+        contracts = contractService.findAllContractWithPagination(id, customerName,pageable);
+        if(contracts.isEmpty()){
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
         }
-        if (contracts.isEmpty()) {
-            return new ResponseEntity<List<Contract>>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(contracts, HttpStatus.OK);
+        return new ResponseEntity<>(contracts,HttpStatus.OK);
     }
+    @DeleteMapping("/list/delete/{id}") //delete contract//
+    public ResponseEntity<String> deleteContractById(@PathVariable("id") String id, Model model) {
+        Contract contract = contractService.findById(id);
+        if(contract == null) {
+            return new ResponseEntity<>("Không tìm thấy hợp đồng" ,HttpStatus.NOT_FOUND);
+        }
+        contractService.deleteContractById(id);
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+//    @GetMapping("/list")
+//    public ResponseEntity<List<Contract>> listAllContract(@RequestParam("id") Optional<String> id, @RequestParam("customerName") Optional<String> customerName) {
+//        List<Contract> contracts = contractService.findAll();
+//        if(id.isPresent() && !customerName.isPresent()) {
+//            contracts = contractService.findByIdContaining(id.get());
+//        } else if(!id.isPresent() && customerName.isPresent()) {
+//            contracts = contractService.findByCustomerName(customerName.get());
+//        } else if(id.isPresent()) {
+//            contracts = contractService.findByIdAndCustomerName(id.get(),customerName.get());
+//        }
+//        if (contracts.isEmpty()) {
+//            return new ResponseEntity<List<Contract>>(HttpStatus.NO_CONTENT);
+//        }
+//        return new ResponseEntity<>(contracts, HttpStatus.OK);
+//    }
 //    @GetMapping("/list")
 //    public ResponseEntity<List<Contract>> listAllContract() {
 //        List<Contract> contracts = contractService.findAll();
@@ -42,15 +66,6 @@ public class ContractController {
 //        return new ResponseEntity<>(contracts, HttpStatus.OK);
 //    }
 
-    @DeleteMapping("/list/delete/{id}") //delete contract//
-    public ResponseEntity<String> deleteContractById(@PathVariable("id") String id, Model model) {
-            Contract contract = contractService.findById(id);
-        if(contract == null) {
-            return new ResponseEntity<>("Không tìm thấy hợp đồng" ,HttpStatus.NOT_FOUND);
-        }
-        contractService.deleteContractById(id);
-        return new ResponseEntity<>("OK", HttpStatus.OK);
-    }
 
 //    @GetMapping("/list/search/{id}/{tenKhachHang}")
 //    public ResponseEntity<List<Contract>> findContract(@PathVariable("id","customerName") String id, String customerName, Model model) {
--- BE/src/main/java/com/example/sprint_1/service/impl/contract/ContractServiceImpl.java
+++ BE/src/main/java/com/example/sprint_1/service/impl/contract/ContractServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.sprint_1.entity.contract.Contract;
 import com.example.sprint_1.repository.contract.ContractRepository;
 import com.example.sprint_1.service.contract.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,14 +15,6 @@ import java.util.List;
 public class ContractServiceImpl implements ContractService {
     @Autowired
     ContractRepository contractRepository;
-
-    @Override
-    public List<Contract> findAll() {
-
-        List<Contract> contracts = contractRepository.findAllContract();
-        return filterContract(contracts);
-    }
-
     @Override
     public void deleteContractById(String id) {
         contractRepository.deleteContract(id);
@@ -32,34 +26,45 @@ public class ContractServiceImpl implements ContractService {
     }
 
     @Override
-    public List<Contract> filterContract(List<Contract> contracts) {
-        List<Contract> contractList = new ArrayList<>();
-        for (Contract contract : contracts) {
-            if (contract.getDeleteFlag()) {
-                contractList.add(contract);
-            }
-        }
-        return contractList;
+    public Page<Contract> findAllContractWithPagination(String id, String customerName, Pageable pageable) {
+        return contractRepository.findAllContractWithPagination(id, customerName, pageable);
     }
+//    @Override
+//    public List<Contract> findAll() {
+//
+//        List<Contract> contracts = contractRepository.findAllContract();
+//        return filterContract(contracts);
+//    }
+
+//    @Override
+//    public List<Contract> filterContract(List<Contract> contracts) {
+//        List<Contract> contractList = new ArrayList<>();
+//        for (Contract contract : contracts) {
+//            if (contract.getDeleteFlag()) {
+//                contractList.add(contract);
+//            }
+//        }
+//        return contractList;
+//    }
 
     //
-    @Override
-    public List<Contract> findByIdAndCustomerName(String id, String customerName) {
-        List<Contract> contracts = contractRepository.findByContractIdContainingAndCustomerNameContaining(id, customerName);
-        return filterContract(contracts);
-    }
-
-    @Override
-    public List<Contract> findByIdContaining(String id) {
-        List<Contract> contracts = contractRepository.findByContractIdContaining(id);
-        return filterContract(contracts);
-    }
-
-    @Override
-    public List<Contract> findByCustomerName(String customerName) {
-        List<Contract> contracts = contractRepository.findByCustomerNameContaining(customerName);
-        return filterContract(contracts);
-    }
+//    @Override
+//    public List<Contract> findByIdAndCustomerName(String id, String customerName) {
+//        List<Contract> contracts = contractRepository.findByContractIdContainingAndCustomerNameContaining(id, customerName);
+//        return filterContract(contracts);
+//    }
+//
+//    @Override
+//    public List<Contract> findByIdContaining(String id) {
+//        List<Contract> contracts = contractRepository.findByContractIdContaining(id);
+//        return filterContract(contracts);
+//    }
+//
+//    @Override
+//    public List<Contract> findByCustomerName(String customerName) {
+//        List<Contract> contracts = contractRepository.findByCustomerNameContaining(customerName);
+//        return filterContract(contracts);
+//    }
 
 //    @Override
 //    public List<Contract> searchContract() {
