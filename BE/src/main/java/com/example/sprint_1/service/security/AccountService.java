8086a853de859903847bef8ff7950b225e905fbb
Lê Công Hậu <73872511+hau2@users.noreply.github.com>
2022-03-22 17:45:54 +0700

    Update AccountService.java

--- BE/src/main/java/com/example/sprint_1/service/security/AccountService.java
+++ BE/src/main/java/com/example/sprint_1/service/security/AccountService.java
@@ -1,6 +1,7 @@
 package com.example.sprint_1.service.security;
 
 import com.example.sprint_1.entity.security.Account;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
 //import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
 
 //import javax.mail.MessagingException;
@@ -19,35 +20,26 @@ public interface AccountService {
 
     Boolean existsById(Integer bookId);
 
-//    void addNew(String username, String password, String email, String token) throws MessagingException, UnsupportedEncodingException;
+    void addNew(String username, String password, String email, String token) throws MessagingException, UnsupportedEncodingException;
 
     Boolean findAccountByVerificationCode(String code);
 
     Boolean findAccountByVerificationCodeToResetPassword(String code);
 
-//    void addVerificationCode(String username) throws MessagingException, UnsupportedEncodingException;
+    void addVerificationCode(String username) throws MessagingException, UnsupportedEncodingException;
 
     List<Account> getAllAccount();
 
-<<<<<<< HEAD
     void addNew(String username, String password);
-=======
-    /*
-    HauLC
-     */
-    void addNew(String username, String email, String password);
->>>>>>> 72757406d8117924b4411b003cf435dbbb361414
 
     void saveNewPassword(String password, String code);
 
     Boolean existsByEmail(String email);
 
-<<<<<<< HEAD
-=======
     /*
-        HauLC
+     *  HauLC
      */
+    void addNew(String username, String email, String password);
     void updateAccount(String password, String email,String username);
 
->>>>>>> 72757406d8117924b4411b003cf435dbbb361414
 }
