c13e5cbf44555040387067e3c8a874306f249ae6
A0321I1 - Hồ Quốc Kiên <hquockien@gmail.com>
2022-03-28 19:54:35 +0700

    a

--- BE/src/main/java/com/example/sprint_1/service/contract/ContractService.java
+++ BE/src/main/java/com/example/sprint_1/service/contract/ContractService.java
@@ -1,7 +1,6 @@
 package com.example.sprint_1.service.contract;
 
 import com.example.sprint_1.dto.contract.ContractDTO;
-import com.example.sprint_1.entity.contract.Contract;
 import com.example.sprint_1.entity.customer.Customer;
 import com.example.sprint_1.entity.ground.Ground;
 import org.springframework.stereotype.Service;
