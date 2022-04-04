d6d808ea6e4a8f317f2616ee20d0957050d30371
duongdaiphuoc0501 <phuocrider25@gmail.com>
2022-04-04 09:38:36 +0700

    PhuocDD push code 4/4

--- BE/src/main/java/com/example/sprint_1/controller/FloorController.java
+++ BE/src/main/java/com/example/sprint_1/controller/FloorController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Console;
+
 
 @RestController
 @RequestMapping("api/home/floor")
@@ -44,3 +46,4 @@ public class FloorController {
 
     }
 }
+
